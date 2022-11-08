a4ff885bc7fdbaa39841d00b8d7422c08763764c
unknown <rijvanshaikh888@gmail.com>
2022-11-08 12:57:26 +0530

    change package and DB

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/server', { useNewUrlParser: true, useUnifiedTopology: true }).then((result) => {
+mongoose.connect('mongodb+srv://Rijvan:Rijvan@node.b2gedvi.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true }).then((result) => {
     console.log('DB COnnected!!!');
 }).catch((err) => {
     console.log('err :', err);
